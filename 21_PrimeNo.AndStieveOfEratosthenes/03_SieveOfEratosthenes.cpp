3c0845f63f27a444e151a044cf9429cd4c26400a
Pranshu51 <pranshutiwari560@gmail.com>
2026-04-16 01:33:03 +0530

    SieveOfErastosthenes

--- 21_PrimeNo.AndStieveOfEratosthenes/03_SieveOfEratosthenes.cpp
+++ 21_PrimeNo.AndStieveOfEratosthenes/03_SieveOfEratosthenes.cpp
@@ -43,3 +43,61 @@
 //         return count;
 //     }
 // };
+
+
+
+
+
+// 2521. Distinct Prime Factors of Product of Array
+// Given an array of positive integers nums, return the number of distinct prime factors in the product of the elements of nums.
+// Note that:
+// A number greater than 1 is called prime if it is divisible by only 1 and itself.
+// An integer val1 is a factor of another integer val2 if val2 / val1 is an integer.
+
+// Example 1:
+// Input: nums = [2,4,3,7,10,6]
+// Output: 4
+// Explanation:
+// The product of all the elements in nums is: 2 * 4 * 3 * 7 * 10 * 6 = 10080 = 25 * 32 * 5 * 7.
+// There are 4 distinct prime factors so we return 4.
+
+
+
+// class Solution {
+// public:
+//     void fillSieve(vector<int>& sieve){
+//         int n=sieve.size()-1;
+//         for(int i=2;i<=sqrt(n);i++){
+//             for(int j=i*2;j<=n;j+=i){
+//                 sieve[j]=0;
+//             }
+//         }
+//     }
+//     int distinctPrimeFactors(vector<int>& nums) {
+//         int n=nums.size();
+//         int mx=-1;
+//         for(int i=0;i<n;i++){
+//             mx=max(mx,nums[i]);
+//         }
+//         vector<int> sieve(mx+1,1);
+//         if(mx+1>0) sieve[0]=0;
+//         if(mx+1>1) sieve[1]=0;
+//         fillSieve(sieve);
+//         vector<int> prime;
+//         for(int i=0;i<=mx;i++){
+//             if(sieve[i]==1) prime.push_back(i);
+//         }
+//         vector<int> taken(prime.size(),0);
+//         for(int i=0;i<nums.size();i++){
+//             for(int j=0;j<prime.size();j++){
+//                 if(prime[j]>nums[i]) break;
+//                 if(nums[i]%prime[j]==0) taken[j]=1;
+//             }
+//         }
+//         int count=0;
+//         for(int i=0;i<taken.size();i++){
+//             if(taken[i]==1) count++;
+//         }
+//         return count;
+//     }
+// };
