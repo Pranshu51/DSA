22d9f389ec9a76f011f79b9ffc487b56729ca3d0
Pranshu51 <pranshutiwari560@gmail.com>
2026-04-17 01:49:17 +0530

    oop basics

---
+++ 22_OOP/01_basics.cpp
@@ -0,0 +1,109 @@
+//WHAT IS OOP?
+//Object Oriented programming is a methodology or paradigm to design a program using classes and objects. It simplifies software development and maintenance by providing some concepts:
+//1. Encapsulation: It is the mechanism of hiding the internal state of an object and requiring all interaction to be performed through an object's methods. It helps to protect the data from unauthorized access and modification.
+//3. Polymorphism: It is the ability of a function, object, or method to take on multiple forms. It allows for the use of a single interface to represent different types of objects, enabling flexibility and extensibility in code design.
+//4. Abstraction: It is the process of hiding the complex implementation details and showing only the necessary features of an object. It helps to reduce complexity and allows the programmer to focus on interactions at a higher level.
+//2. Inheritance: It is the mechanism of creating a new class from an existing class. The new class inherits the properties and behaviors of the existing class, allowing for code reusability and the creation of a hierarchical relationship between classes.
+//Muliple cheezo ko a single unit me daal dia 
+
+
+
+//WHAT ARE CLASSES?(BluePrint of an object)
+//class is a user-defined data type that serves as a blueprint for creating objects. It encapsulates data members (attributes) and member functions (methods) that operate on the data. A class defines the properties and behaviors of the objects that can be created from it. It provides a way to model real-world entities and their interactions in a program.
+
+//calss Player{
+//private: //access specifier
+// int health; //data members or attributes
+//public:
+// int score;
+// string name;
+// }
+
+
+//Player pranshu; //object of class Player
+
+//ACCESS MODIFIERS
+//1. Public: Members declared as public are accessible from anywhere in the program. They can be accessed by any code that has visibility to the class, including outside the class and within derived classes.
+//Protected: Members declared as protected are accessible within the class itself and by derived classes. They cannot be accessed from outside the class, but they can be accessed by classes that inherit from the class in which they are declared.
+//2. Private: Members declared as private are only accessible within the class itself. They cannot
+
+#include<iostream>
+using namespace std;
+
+
+// class Player{
+//     public: //access specifier
+// int health;
+// int score;
+// string name;
+// void showhealth(){//member function
+//     cout<<"Player health : "<<health<<endl;
+// }
+// };
+
+// int main(){
+// Player pranshu; //object of class Player
+// pranshu.health = 100;
+// pranshu.score = 0;
+// pranshu.name = "Pranshu";
+// cout<<"Player name : "<<pranshu.name<<endl;
+// pranshu.showhealth();
+// cout<<"Player score : "<<pranshu.score<<endl;
+
+// Player pranshu2; //object of class Player
+// pranshu2.health = 90;
+// pranshu2.score = 10;
+// pranshu2.name = "Pranshu2";
+// Player player[10];
+// player[0] = pranshu;
+// player[1] = pranshu2;
+// }
+//o/p:
+// Player name : Pranshu
+// Player health : 100
+// Player score : 0
+
+
+//CLASS VARIABLES
+//declaration, intialization and access of class variables
+
+//->data members or attributes of a class are called class variables. They are used to store the state of an object. They can be of any data type and can be accessed using the object of the class. They can also be initialized using constructors or directly in the class definition.
+//member functions->  class ke ander ke functions ko member functions kehte hai. They are used to perform operations on the data members of the class. They can be defined inside or outside the class definition and can be accessed using the object of the class. They can also be called using the scope resolution operator (::) if they are defined outside the class definition.
+
+// class Calculator{
+//     public:
+//     int a,b;//data members or attributes of class Calculator
+//     void add(){//member function of class Calculator
+//         cout<<"Sum : "<<a+b<<endl;
+//     }
+
+// };
+
+// int main(){
+//     Calculator calc; //object of class Calculator
+//     calc.a = 10;
+//     calc.b = 20;
+//     calc.add();
+// }
+
+
+//Getter and Setter
+
+class Player{
+    private:
+    int health; //data members or attributes of class Player
+    public:
+    void setHealth(int h){ //setter function to set the value of health
+        cout<<"health set kiya jaa rha h"<<endl;//hum geteer setter ki  mada se track akr skte h ki health set kiya jaa rha h ya nahi
+        health = h;
+    }
+    int getHealth(){ //getter function to get the value of health
+        return health;
+    }
+};
+
+int main(){
+    Player pranshu; //object of class Player
+    pranshu.setHealth(100); //setting the value of health using setter function
+    cout<<"Player health : "<<pranshu.getHealth()<<endl; //getting the value of health using getter function
+}
---
+++ 22_OOP/02_oop2.cpp
@@ -0,0 +1,179 @@
+#include<iostream>
+using namespace std;
+
+//PASSING CLASS OBJECTS TO FUNCTIONS
+class Gun{
+    public:
+    int ammo;
+    int damage;
+    int scope;
+};
+class Player{
+    private:
+     class Helmet{
+        public: // Made public so Player can access these through functions
+        int hp;
+        int level;
+
+        // Added these to match the logic in your setHelmet function
+        void setHp(int hp) { this->hp = hp; }
+        void setLevel(int level) { this->level = level; }
+        int getHp() { return hp; }
+        int getLevel() { return level; }
+    };
+
+ private:
+    int health;
+    int age;
+    int score;
+    bool alive;
+    Gun gun;//iski class bahar h object ander lia hua h 
+    Helmet helmet;
+
+public:
+    int getHealth(){
+        return health;
+    }
+    int getAge(){
+        return age;
+    }
+    int getScore(){
+        return score;
+    }
+    int getAlive(){
+        return alive;
+    }
+
+    Gun getGun(){
+        return gun;
+    }
+    
+    void setHealth(int health){
+        this->health = health;
+    }
+    void setAge(int age){
+        this->age = age;
+    }
+    void setScore(int score){
+        this->score = score;
+    }
+    void setAlive(bool alive){
+        this->alive = alive; 
+    }
+    void setGun(Gun gun){
+        this->gun = gun;
+    }
+
+    public:
+    // --- INTEGRATED FROM SCREENSHOT ---
+    void setHelmet(int level){
+        Helmet *helmet = new Helmet; // Dynamic allocation
+        helmet->setLevel(level);
+        int health = 0;
+
+        if(level == 1)
+            health = 25;
+        else if(level == 2)
+            health = 50;
+        else if(level == 3)
+            health = 100;
+        else
+            cout << "error, invalid level!!";
+
+        helmet->setHp(health);
+        this->helmet = *helmet; // Store the helmet in the class member
+        
+        delete helmet; // Added this to prevent the memory leak mentioned in your comment!
+        
+        // Note: Since 'helmet' is a local pointer, it will leak memory 
+        // unless you delete it here or store it as a class member!
+    }
+    void getHelmet(){
+        cout<<"Helmet Level: "<<helmet.getLevel()<<endl;
+        cout<<"Helmet HP: "<<helmet.getHp()<<endl;
+    }
+};
+
+int addScore(Player p1, Player p2){
+    int totalScore = p1.getScore() + p2.getScore();
+        return totalScore;
+}
+
+Player getMaxScorePlayer(Player p1, Player p2){
+    if(p1.getScore() > p2.getScore()){
+        return p1;
+    }
+    else{
+        return p2;
+    }
+}
+
+int main(){//creating objects of the class Player so it is also a type of data type
+    Player pranshu;//MEMORY ALLOCATION STATICALLY
+    pranshu.setHealth(100);
+    pranshu.setAge(20);
+    pranshu.setScore(1000);
+    pranshu.setAlive(true);
+    pranshu.setHelmet(2);
+
+    Player rishav;//MEMORY ALLOACATION STATICALLY
+    rishav.setHealth(90);
+    rishav.setAge(21);
+    rishav.setScore(900);
+    rishav.setAlive(true);
+    rishav.setHelmet(3);
+
+    Gun akm;
+    akm.ammo = 30;
+    akm.damage = 50;
+    akm.scope = 4;
+
+    Gun awm;
+    awm.ammo = 5;
+    awm.damage = 100;
+    awm.scope = 8;
+
+    Gun gun1 = pranshu.getGun();
+    cout<<"Pranshu's Gun Ammo: "<<gun1.ammo<<endl;
+    cout<<"Pranshu's Gun Damage: "<<gun1.damage<<endl;
+    cout<<"Pranshu's Gun Scope: "<<gun1.scope<<endl;
+
+    pranshu.getHelmet();
+
+    Player *king = new Player;//MEMORY ALLOCATION DYNAMICALLY
+    //best way to set values for dynamically allocated objects is to use pointer notation
+    king->setHealth(80);
+    cout<<"kingHealthWithoutObject: "<<king->getHealth()<<endl;
+
+
+    // Player kingObject = *king;//DYNAMICALLY ALLOCATED OBJECT
+    // //agr bina object banye direct karna to *king.setHealth(80); //ye bhi sahi hoga
+    // (*king).setHealth(80); //ye bhi sahi hoga
+    // cout<<"kingHealthWithoutObject: "<<(*king).getHealth()<<endl;
+    // kingObject.setHealth(80);
+    // cout<<"kingHealthWithObject: "<<kingObject.getHealth()<<endl;
+    // kingObject.setAge(22);//dynamically allocated object ke liye bhi hum same tarike se set kar sakte hai
+    // kingObject.setScore(800);
+
+    // cout<<addScore(pranshu, rishav);
+    // Player Sachin = getMaxScorePlayer(pranshu, rishav);
+    // cout<<endl<<"Player with maximum score is: "<<Sachin.getScore()<<endl;
+
+    // delete king; // Deallocating the dynamic Player object
+
+
+
+    //Array of objects
+    Player players[10]={pranshu,rishav}; //array of 10 Player objects allocated statically
+    cout<<"Player 1 Health: "<<players[0].getHealth()<<endl;
+    cout<<"Player 2 Health: "<<players[1].getHealth()<<endl;
+}
+
+
+
+//STATIC ALLOCATION VS DYNAMIC ALLOCATION OF OBJECTS
+
+//STATIC ALLOCATION(stack memory)=>1. Ye compile time memory allocation h.Ye program start hone se pele RAM me store ho jati h iska mtlb hota hai ki hum class ke objects ko stack memory me allocate krte hai, aur jab hum function se bahar nikalte hai to wo automatically destroy ho jate hai.
+//DYNAMIC ALLOCATION(heap memory->Real time)=>Ye runtime memory allocaiton h . Ye program chlne ke dauran allocate hoti h  iska mtlb hota hai ki hum class ke objects ko heap memory me allocate krte hai, aur humein manually unhe deallocate karna padta hai.
+//DM=> eg->int *ptr = new int; //dynamic memory allocation for an integer
+//delete ptr; //deallocating the memory correct this code with including my comments
