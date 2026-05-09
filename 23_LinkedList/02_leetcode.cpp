07fbb23d56a2d88b215f3c773b8f6e984f3e45ca
Pranshu51 <pranshutiwari560@gmail.com>
2026-05-09 20:35:41 +0530

    LLQuesiotn

--- 23_LinkedList/02_leetcode.cpp
+++ 23_LinkedList/02_leetcode.cpp
@@ -253,3 +253,81 @@
         
 //     }
 // };
+
+
+
+
+
+
+// 141. Linked List Cycle
+
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+//  Input: head = [3,2,0,-4], pos = 1
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         ListNode * slow=head;
+//         ListNode * fast=head;
+//         while(fast!=NULL && fast->next !=NULL){
+//             slow=slow->next;
+//             fast=fast->next->next;
+//             if(slow==fast) return true;
+//         }
+//         return false;
+//     }
+// };
+
+
+
+
+
+
+
+// 142. Linked List Cycle II
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+// Do not modify the linked list.
+// Example 1:
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: tail connects to node index 1
+// Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//          ListNode * slow=head;
+//         ListNode * fast=head;
+//         bool flag=false;
+//         while(fast!=NULL && fast->next !=NULL){
+//             slow=slow->next;
+//             fast=fast->next->next;
+//             if(slow==fast) {
+//                 flag=true;
+//                 break;
+//             }
+//         }
+//         if(flag==false) return NULL;
+//         else{
+//             ListNode* temp =head;
+//             while(temp!=slow){
+//                  slow=slow->next;
+//                 temp=temp->next;
+//             }
+//         }
+//         return slow;//return temp;
+//     }
+// };
