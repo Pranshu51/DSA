e035a7c24f65e4e36a7e1f7f18fc7dde3901ff99
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-05-16 16:56:24 +0530

    updatedLL

--- 23_LinkedList/03_leetcode.cpp
+++ 23_LinkedList/03_leetcode.cpp
@@ -69,7 +69,7 @@
 // 2326. Spiral Matrix IV
 
 // You are given two integers m and n, which represent the dimensions of a matrix.
-// You are also given the head of a linked list of integers\
+// You are also given the head of a linked list of integers
 // Generate an m x n matrix that contains the integers in the linked list presented in spiral order (clockwise), starting from the top-left of the matrix. If there are remaining empty spaces, fill them with -1.
 // Return the generated matrix.
 
@@ -679,7 +679,7 @@
 
 
 
-// 92. Reverse Linked List II
+// 92. Reverse Linked List II(Reverse part of list)
 
 // Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
 
