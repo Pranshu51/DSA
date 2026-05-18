d6bbca25343dacdfc83a0e6a0ccf917b13683a33
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-05-18 11:05:20 +0530

    LLQue

--- 23_LinkedList/05_leetcode.cpp
+++ 23_LinkedList/05_leetcode.cpp
@@ -314,4 +314,94 @@
 
 
 
+// 138. Copy List with Random Pointer
+
+// A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+// Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+// For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
+// Return the head of the copied linked list.
+// The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+// val: an integer representing Node.val
+// random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
+// Your code will only be given the head of the original linked list.
+
+ 
+
+// Example 1:
+// Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+// Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+
+
+// /*
+// // Definition for a Node.
+// class Node {
+// public:
+//     int val;
+//     Node* next;
+//     Node* random;
+// };
+// */
+
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         //step-1. deep copy without random just make straight connections
+//         Node* temp =head;
+//         Node* dummy =new Node(0);
+//         Node* tempC= dummy;
+//         while(temp){
+//             Node* a =new Node(temp->val);
+//             tempC->next =a;
+//             tempC=tempC->next;
+//             temp=temp->next;
+//         }
+//         Node* duplicate = dummy->next;
+
+//         //step-2 Alternate connections
+//         Node* a=head;
+//         Node* b=duplicate;
+//         dummy = new Node(10);
+//         Node* tempD= dummy;
+//         while(a){
+//             tempD->next=a;
+//             a=a->next;
+//             tempD=tempD->next;
+
+//             tempD->next=b;
+//             b=b->next;
+//             tempD=tempD->next;
+//         }
+//         dummy=dummy->next;
+//         //step-3 assigning duplicate pointers of duplicate
+//         Node* t1 =dummy;
+//         while(t1){
+//             Node* t2=t1->next;
+//             if(t1->random) t2->random=t1->random->next;
+//             t1 =t1->next->next;
+//         }
+//         //step-4 Removing the connections(separating)
+//         Node* d1 = new Node(10);
+//         Node* d2 = new Node(10);
+//          t1 = d1;
+//         Node* t2 = d2;
+//         Node* t= dummy;
+//         while(t){
+//             t1->next= t;
+//             t=t->next;
+//             t1=t1->next;
+
+//             t2->next= t;
+//             t=t->next;
+//             t2=t2->next;
+//         }
+//         t1->next =NULL;
+//         t2->next =NULL;
+//         d1=d1->next;
+//         d2=d2->next;
+//         return d2;
+//     }
+// };
+
+
+
 
