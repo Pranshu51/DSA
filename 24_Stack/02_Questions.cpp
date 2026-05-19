9f592ce7f976a5d15e0c4bc7207d7e75aa38c47a
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-05-19 12:53:08 +0530

    satck2

--- 24_Stack/02_Questions.cpp
+++ 24_Stack/02_Questions.cpp
@@ -187,25 +187,78 @@ using namespace std;
 
 
 
-int main(){
-    int arr[] = {3,2,5,6,7,5,6,9,4};
-    int n= sizeof(arr)/sizeof(arr[0]);
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-    stack<int> st;
-    int pgi[n];
-    pgi[0]=1;
-    st.push(0);
-    for(int i=1;i<n;i++){
-        while(st.size()>0 && arr[st.top()]<=arr[i]) st.pop();
-        if(st.size()==0) pgi[i]=-1;
-        else pgi[i]=st.top();
-        pgi[i]=i-pgi[i];
-        st.push(i);
-    }
-     for(int i=0;i<n;i++){
-        cout<<pgi[i]<<" ";
-    }
-}
+// int main(){
+//     int arr[] = {3,2,5,6,7,5,6,9,4};
+//     int n= sizeof(arr)/sizeof(arr[0]);
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+//     stack<int> st;
+//     int pgi[n];
+//     pgi[0]=1;
+//     st.push(0);
+//     for(int i=1;i<n;i++){
+//         while(st.size()>0 && arr[st.top()]<=arr[i]) st.pop();
+//         if(st.size()==0) pgi[i]=-1;
+//         else pgi[i]=st.top();
+//         pgi[i]=i-pgi[i];
+//         st.push(i);
+//     }
+//      for(int i=0;i<n;i++){
+//         cout<<pgi[i]<<" ";
+//     }
+// }
+
+
+
+
+
+// 84. Largest Rectangle in Histogram
+
+// Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+ 
+
+// Example 1:
+// Input: heights = [2,1,5,6,2,3]
+// Output: 10
+// Explanation: The above is a histogram where width of each bar is 1.
+// The largest rectangle is shown in the red area, which has an area = 10 units.
+
+
+// class Solution {
+// public:
+//     int largestRectangleArea(vector<int>& heights) {
+//         int n =heights.size();
+//         stack<int> st;
+//         vector<int> psi(n);
+//         psi[0]=-1;
+//         st.push(0);
+//         for(int i=1;i<n;i++){
+//             while(st.size()>0 && heights[st.top()]>=heights[i] ) st.pop();
+//             if(st.size()==0) psi[i] = -1;
+//             else psi[i] = st.top();
+//              st.push(i);
+//         }
+
+//         stack<int> gt;
+//         vector<int> nsi(n);
+//         nsi[n-1]=n;
+//         gt.push(n-1);
+//         for(int i=n-2;i>=0;i--){
+//             while(gt.size()>0 && heights[gt.top()]>=heights[i] ) gt.pop();
+//             if(gt.size()==0) nsi[i] = n;
+//             else nsi[i] = gt.top();
+//              gt.push(i);
+//         }
+//         int maxArea=0;
+//         for(int i=0;i<heights.size();i++){
+//             int height= heights[i];
+//             int breadth= nsi[i]-psi[i]-1;
+//             int area = height*breadth;
+//             maxArea=max(maxArea,area);
+//         }
+//         return maxArea;
+//     }
+// };
