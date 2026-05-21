a09663677924ecafed8f323620b05630642c4601
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-05-21 20:18:39 +0530

    stack

--- 24_Stack/03_leetcode.cpp
+++ 24_Stack/03_leetcode.cpp
@@ -95,3 +95,126 @@
 //         return ans;
 //     }
 // };
+
+
+
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        if(k==1) return arr;
+        int n = arr.size();
+        int ngi[n];
+        stack<int> st;
+        ngi[n-1] =n;
+        st.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            while(st.size()>0 && arr[st.top()]<=arr[i]) st.pop();
+            if(st.size()==0) ngi[i] =n;
+            else ngi[i]=st.top();
+            st.push(i);
+        }
+        vector<int> ans;
+        int j=0;
+        for(int i=0;i<n-k+1;i++){
+            if(j<i) j=i;
+            int mx=arr[j];
+            int j=i;
+            while(j<i+k){
+                mx=arr[j];
+                if(ngi[i]>=i+k) break;
+                j=ngi[j];
+            }
+            ans.push_back(mx);
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+155. Min Stack
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+
+
+
+
+class MinStack {
+public:
+stack<int> st;
+stack<int> helper;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(helper.size()==0 || val<helper.top()) helper.push(val);
+        else helper.push(helper.top());
+    }
+    
+    void pop() {
+        st.pop();
+        helper.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return helper.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
