e629bb859a5abd352d9fc9d0c1c365d000028d12
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-05-25 01:54:58 +0530

    Starting Queue

---
+++ 25_Queue/01_Basic.cpp
@@ -0,0 +1,10 @@
+//QUEUE INTRODUCITON
+
+//FIFO - FIRST IN FIRST OUT
+//Pop is done from the front and push is done from the back/rear
+
+//stack vs queue
+//STACK - LIFO - LAST IN FIRST OUT
+// st-> push and pop is done from the same end (top)
+//QUEUE - FIFO - FIRST IN FIRST OUT
+// q-> push is done from the rear and pop is done from the front
