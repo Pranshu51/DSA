c85cb1300ef4b45d1ec026ed244b758202e210da
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-05-29 13:32:26 +0530

    stackUpdated

--- 24_Stack/03_leetcode.cpp
+++ 24_Stack/03_leetcode.cpp
@@ -141,11 +141,7 @@
 
 
 // 155. Min Stack
-// Medium
-// Topics
-// premium lock icon
-// Companies
-// Hint
+
 // Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
 // Implement the MinStack class:
@@ -210,19 +206,13 @@
 //     }
 // };
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
 
 
 
 
 //  //alternate bad app
+
+
 //  class MinStack {
 // public:
 // vector<int> v;
