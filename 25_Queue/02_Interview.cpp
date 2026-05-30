169ac377e6bd48db8d423a283a346402a8f259f2
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-05-31 00:28:17 +0530

    QueueDone

--- 25_Queue/02_Interview.cpp
+++ 25_Queue/02_Interview.cpp
@@ -615,55 +615,114 @@ Output: 11 16 12 17 13 18 14 19 15 20
  */
 
 //  (do this using satck only) if coding round then do it with array
-#include<algorithm>
-void display(queue<int> &q){
-       int n=q.size();
-    for(int i=0;i<n;i++){
-        cout<<q.front();
-        q.push(q.front());
-        q.pop();
-    }
-}
-void reorder(queue<int> &q){
-        stack<int> st;
-        int n=q.size();
-    while(q.size()>n/2){
-        st.push(q.front());
-        q.pop();
-    }
-    while(st.size()>0){
-        q.push(st.top());
-        st.pop();
-    }
-    while(q.size()>n/2){
-        st.push(q.front());
-        q.pop();
-    }
-    while(st.size()>0){
-        q.push(st.top());
-        st.pop();
-        q.push(q.front());
-        q.pop();
-    }
-    while(q.size()>0){
-        st.push(q.front());
-        q.pop();
-    }
-    while(st.size()>0){
-        q.push(st.top());
-        st.pop();
-    }
-}
-int main(){
-    queue<int> q;
-    q.push(1);
-    q.push(2);
-    q.push(3);
-    q.push(4);
+// #include<algorithm>
+// void display(queue<int> &q){
+//        int n=q.size();
+//     for(int i=0;i<n;i++){
+//         cout<<q.front();
+//         q.push(q.front());
+//         q.pop();
+//     }
+// }
+// void reorder(queue<int> &q){
+//         stack<int> st;
+//         int n=q.size();
+//     while(q.size()>n/2){
+//         st.push(q.front());
+//         q.pop();
+//     }
+//     while(st.size()>0){
+//         q.push(st.top());
+//         st.pop();
+//     }
+//     while(q.size()>n/2){
+//         st.push(q.front());
+//         q.pop();
+//     }
+//     while(st.size()>0){
+//         q.push(st.top());
+//         st.pop();
+//         q.push(q.front());
+//         q.pop();
+//     }
+//     while(q.size()>0){
+//         st.push(q.front());
+//         q.pop();
+//     }
+//     while(st.size()>0){
+//         q.push(st.top());
+//         st.pop();
+//     }
+// }
+// int main(){
+//     queue<int> q;
+//     q.push(1);
+//     q.push(2);
+//     q.push(3);
+//     q.push(4);
 
-    display(q);
-    cout<<endl;
-    reorder(q);
-    display(q);
+//     display(q);
+//     cout<<endl;
+//     reorder(q);
+//     display(q);
 
-}
+// }
+
+
+
+
+
+
+// 950. Reveal Cards In Increasing Order
+
+// You are given an integer array deck. There is a deck of cards where every card has a unique integer. The integer on the ith card is deck[i].
+
+// You can order the deck in any order you want. Initially, all the cards start face down (unrevealed) in one deck.
+
+// You will do the following steps repeatedly until all cards are revealed:
+
+// Take the top card of the deck, reveal it, and take it out of the deck.
+// If there are still cards in the deck then put the next top card of the deck at the bottom of the deck.
+// If there are still unrevealed cards, go back to step 1. Otherwise, stop.
+// Return an ordering of the deck that would reveal the cards in increasing order.
+
+// Note that the first entry in the answer is considered to be the top of the deck.
+
+ 
+
+// Example 1:
+
+// Input: deck = [17,13,11,2,3,5,7]
+// Output: [2,13,3,11,5,17,7]
+// Explanation: 
+// We get the deck in the order [17,13,11,2,3,5,7] (this order does not matter), and reorder it.
+// After reordering, the deck starts as [2,13,3,11,5,17,7], where 2 is the top of the deck.
+// We reveal 2, and move 13 to the bottom.  The deck is now [3,11,5,17,7,13].
+// We reveal 3, and move 11 to the bottom.  The deck is now [5,17,7,13,11].
+// We reveal 5, and move 17 to the bottom.  The deck is now [7,13,11,17].
+// We reveal 7, and move 13 to the bottom.  The deck is now [11,17,13].
+// We reveal 11, and move 17 to the bottom.  The deck is now [13,17].
+// We reveal 13, and move 17 to the bottom.  The deck is now [17].
+// We reveal 17.
+// Since all the cards revealed are in increasing order, the answer is correct.
+
+// class Solution {
+// public:
+//     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+//         int n =deck.size();
+//         sort(deck.begin(),deck.end());
+//         queue<int> q;//0-n-1
+//         for(int i=0;i<n;i++){
+//             q.push(i);
+//         }
+//         vector<int> ans(n);
+//         for(int i=0;i<n;i++){
+//             int idx=q.front();
+//             q.pop();
+//             q.push(q.front());
+//             q.pop();
+//             ans[idx]=deck[i];
+//         }
+//         return ans;
+//     }
+// };
