c62004fa13c4cf834fde00b7cf466fa953fc92c5
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-06-03 11:34:48 +0530

    mergeSortUpdated

--- 15_MergeSort/01_Basics.cpp
+++ 15_MergeSort/01_Basics.cpp
@@ -232,3 +232,16 @@ using namespace std;
     
 // }
 
+
+// dont know true or not
+
+// int main(){
+//      int arr[]={5,1,3,0,4,2,6};
+//     int n=sizeof(arr)/sizeof(arr[0]);  
+//     int count=0;  
+//     for(int i=0;i<n;i++){
+//         for(int j=i+1;j<n;j++)
+//         if(arr[i]>arr[j]) count++;
+//     }
+//     cout << "Inversion Count: " << count << endl;
+// }
