be2973ad96619934d236b5870033c6d61d405716
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-19 02:00:13 +0530

    TreeTraversal

--- 26_BinaryTree/02_TreeTreaversals.cpp
+++ 26_BinaryTree/02_TreeTreaversals.cpp
@@ -6,6 +6,134 @@
 using namespace std;
 
 
+// class Node{//This is a tree node
+// public:    
+//     int val;
+//     Node* left;
+//     Node* right;
+//     Node(int val){
+//         this->val=val;
+//         this->left=NULL;
+//         this->right=NULL;
+//     }
+// };
+
+// void preorder(Node* root){
+//     if(root==NULL) return;//base case
+//     cout<<root->val<<" ";//root
+//     preorder(root->left);//left
+//     preorder(root->right);//right
+// }
+
+// void inorder(Node* root){
+//     if(root==NULL) return;//base case
+//     inorder(root->left);//left
+//     cout<<root->val<<" ";//root
+//     inorder(root->right);//right
+// }
+
+// void postorder(Node* root){
+//     if(root==NULL) return;//base case
+//     postorder(root->left);//left
+//     postorder(root->right);//right
+//     cout<<root->val<<" ";//root
+// }
+
+// void nthLevel(Node* root,int curr,int level){
+//     if(root==NULL) return;//base case
+//     if(curr==level){
+//         cout<<root->val<<" ";//root
+//         return;
+//     }
+//     nthLevel(root->left,curr+1,level);//left
+//     nthLevel(root->right,curr+1,level);//right
+// }
+
+// void nthLevelRev(Node* root,int curr,int level){
+//     if(root==NULL) return;//base case
+//     if(curr==level){
+//         cout<<root->val<<" ";//root
+//         return;
+//     }
+//     nthLevelRev(root->right,curr+1,level);//right
+//     nthLevelRev(root->left,curr+1,level);//left
+// }
+
+// int level(Node* root){
+//     if(root==NULL) return 0;//base case
+//     return 1 + max(level(root->left),level(root->right));
+// }
+
+// void levelOrder(Node* root){
+//     int n=level(root);
+//     for(int i=0;i<n;i++){
+//         nthLevel(root,0,i);
+//         cout<<endl;
+//     }
+// }
+
+
+// //BREADTH FIRST SEARCH
+// void levelOrderQueue(Node* root){//BFS
+//     queue<Node*> q;
+//     q.push(root);
+//     while(q.size()>0){
+//         Node* temp=q.front();
+//         q.pop();
+//         cout<<temp->val<<" ";
+//         if(temp->left !=NULL) q.push(temp->left);
+//         if(temp->right !=NULL) q.push(temp->right);
+//     }
+//     cout<<endl;
+// }
+
+// void levelOrderQueueRev(Node* root){
+//     queue<Node*> q;
+//     q.push(root);
+//     while(q.size()>0){
+//         Node* temp=q.front();
+//         q.pop();
+//         cout<<temp->val<<" ";
+//         if(temp->right !=NULL) q.push(temp->right);
+//         if(temp->left !=NULL) q.push(temp->left);
+//     }
+//     cout<<endl;
+// }
+
+// int main(){
+//     Node* a =new Node(1);
+//     Node* b =new Node(2);
+//     Node* c =new Node(3);
+//     Node* d =new Node(4);
+//     Node* e =new Node(5);
+//     Node* f =new Node(6);
+//     Node* g =new Node(7);
+//     a->left=b;
+//     a->right=c;
+//     b->left=d;
+//     b->right=e;
+//     c->left=f;
+//     c->right=g;
+//     // preorder(a);
+//     // cout<<endl;
+//     // inorder(a);
+//     // cout<<endl;
+//     // postorder(a);
+//     // cout<<endl;
+//     // nthLevel(a,0,2);//also start it with 1
+//     cout<<endl;
+//     // levelOrder(a);
+//     levelOrderQueue(a);
+//     levelOrderQueueRev(a);
+// }
+
+
+
+
+
+//LEVEL ORDER TRAVERSAL USING QUEUE(BFS)
+  #include <algorithm>
+  #include <climits>
 class Node{//This is a tree node
 public:    
     int val;
@@ -18,60 +146,33 @@ public:
     }
 };
 
-void preorder(Node* root){
-    if(root==NULL) return;//base case
-    cout<<root->val<<" ";//root
-    preorder(root->left);//left
-    preorder(root->right);//right
-}
+Node* construct(int arr[],int n){
+    queue<Node*> q;
+    Node* root = new Node(arr[0]);
+    q.push(root);
+    int i=1;
+    int j=2;
+    while(q.size()>0 && i<n){
+        Node* temp =q.front();
+        q.pop();
+        Node* l;
+        Node* r;
+        if(arr[i]!=INT_MIN) l = new Node(arr[i]);
+        else l=NULL;
+        if(j!=n && arr[j]!=INT_MIN) r = new Node(arr[j]);
+        else r=NULL;
 
-void inorder(Node* root){
-    if(root==NULL) return;//base case
-    inorder(root->left);//left
-    cout<<root->val<<" ";//root
-    inorder(root->right);//right
-}
+        temp->left = l;
+        temp->right = r;
 
-void postorder(Node* root){
-    if(root==NULL) return;//base case
-    postorder(root->left);//left
-    postorder(root->right);//right
-    cout<<root->val<<" ";//root
-}
-
-void nthLevel(Node* root,int curr,int level){
-    if(root==NULL) return;//base case
-    if(curr==level){
-        cout<<root->val<<" ";//root
-        return;
+        if(l!=NULL) q.push(l);
+        if(r!=NULL) q.push(r);
+        
+        i+=2;
+        j+=2;
     }
-    nthLevel(root->left,curr+1,level);//left
-    nthLevel(root->right,curr+1,level);//right
+    return root;
 }
-
-void nthLevelRev(Node* root,int curr,int level){
-    if(root==NULL) return;//base case
-    if(curr==level){
-        cout<<root->val<<" ";//root
-        return;
-    }
-    nthLevelRev(root->right,curr+1,level);//right
-    nthLevelRev(root->left,curr+1,level);//left
-}
-
-int level(Node* root){
-    if(root==NULL) return 0;//base case
-    return 1 + max(level(root->left),level(root->right));
-}
-
-void levelOrder(Node* root){
-    int n=level(root);
-    for(int i=0;i<n;i++){
-        nthLevel(root,0,i);
-        cout<<endl;
-    }
-}
-
 
 //BREADTH FIRST SEARCH
 void levelOrderQueue(Node* root){//BFS
@@ -87,49 +188,13 @@ void levelOrderQueue(Node* root){//BFS
     cout<<endl;
 }
 
-void levelOrderQueueRev(Node* root){
-    queue<Node*> q;
-    q.push(root);
-    while(q.size()>0){
-        Node* temp=q.front();
-        q.pop();
-        cout<<temp->val<<" ";
-        if(temp->right !=NULL) q.push(temp->right);
-        if(temp->left !=NULL) q.push(temp->left);
-    }
-    cout<<endl;
-}
-
 int main(){
-    Node* a =new Node(1);
-    Node* b =new Node(2);
-    Node* c =new Node(3);
-    Node* d =new Node(4);
-    Node* e =new Node(5);
-    Node* f =new Node(6);
-    Node* g =new Node(7);
-    a->left=b;
-    a->right=c;
-    b->left=d;
-    b->right=e;
-    c->left=f;
-    c->right=g;
-    // preorder(a);
-    // cout<<endl;
-    // inorder(a);
-    // cout<<endl;
-    // postorder(a);
-    // cout<<endl;
-    // nthLevel(a,0,2);//also start it with 1
-    cout<<endl;
-    // levelOrder(a);
-    levelOrderQueue(a);
-    levelOrderQueueRev(a);
+    int arr[]={1,2,3,4,5,INT_MIN,INT_MIN,6,INT_MIN,INT_MIN,7,8,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    Node* root  = construct(arr,n);
+   
+    levelOrderQueue(root);
 }
 
 
 
-
-
-//LEVEL ORDER TRAVERSAL USING QUEUE(BFS)
-  
