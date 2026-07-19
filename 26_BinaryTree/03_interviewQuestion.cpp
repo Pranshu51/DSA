75033dd90e9b64cca2f3e6e1ce34b80f4add2e36
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-19 18:57:53 +0530

    PathSum

--- 26_BinaryTree/03_interviewQuestion.cpp
+++ 26_BinaryTree/03_interviewQuestion.cpp
@@ -341,3 +341,41 @@ using namespace std;
 //         return ans;
 //     }
 // };
+
+
+
+// 113. Path Sum II
+
+// Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+// A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+// Example 1:
+// Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+// Output: [[5,4,11,2],[5,8,4,5]]
+// Explanation: There are two paths whose sum equals targetSum:
+// 5 + 4 + 11 + 2 = 22
+// 5 + 8 + 4 + 5 = 22
+
+
+// class Solution {
+// public:
+//     void helper(TreeNode* root,vector<int> v, vector<vector<int>> &ans,int targetSum){
+//         if(root==NULL) return;
+//         if(root->left==NULL && root->right==NULL){
+//             if(root->val==targetSum){
+//              v.push_back(root->val);
+//             ans.push_back(v);
+//             }
+//             return;
+//         }
+//         v.push_back(root->val);
+//         helper(root->left,v,ans,targetSum-(root->val));
+//         helper(root->right,v,ans,targetSum-(root->val));
+//     }
+//     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+//          vector<vector<int>> ans;
+//          vector<int> v;
+//          helper(root,v,ans,targetSum);
+//          return ans;
+//     }
+// };
