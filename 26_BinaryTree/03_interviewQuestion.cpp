54f0694ef8876ee289287236654ac313f86853db
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-20 08:43:38 +0530

    Binary Tree from Preorder and Inorder Traversal

--- 26_BinaryTree/03_interviewQuestion.cpp
+++ 26_BinaryTree/03_interviewQuestion.cpp
@@ -404,3 +404,36 @@ using namespace std;
 //         return count;
 //     }
 // };
+
+
+
+// 105. Construct Binary Tree from Preorder and Inorder Traversal
+
+// Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+// Example 1:
+// Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+// Output: [3,9,20,null,null,15,7]
+
+// class Solution {
+// public:
+//     TreeNode* build(vector<int>& pre,int prelo,int prehi, vector<int>& in,int inlo,int inhi){
+//         if(prelo>prehi) return NULL;
+//         TreeNode* root = new TreeNode(pre[prelo]);
+//         if(prelo==prehi) return root;
+//         int i=inlo;
+//         while(i<=inhi){
+//             if(in[i]==pre[prelo]) break;
+//             i++;
+//         }
+//         int leftCount=i-inlo;
+//         int rightCount=inhi-i;
+//         root->left=build(pre,prelo+1,prelo+leftCount,in,inlo,i-1);
+//         root->right=build(pre,prelo+leftCount+1,prehi,in,i+1,inhi);
+//         return root;
+//     }
+//     TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
+//         int n= pre.size();
+//         return build(pre,0,n-1,in,0,n-1);
+//     }
+// };
