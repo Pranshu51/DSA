3ad4c2d94224b5dd15e6e7af44473507cc34ccd8
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-21 04:55:36 +0530

    BstBasics

---
+++ 27_BinarySearchTree/01_basics.cpp
@@ -0,0 +1,2 @@
+//What is  Binary Search Tree (BST)?
+//every node in the left subtree of a node has a value less than the node's value, and every node in the right subtree has a value greater than the node's value. This property must hold true for all nodes in the tree.
