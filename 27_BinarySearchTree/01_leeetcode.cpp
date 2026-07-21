e4eb6ff06250f5a4aeca9a8d4eb034287ab2d620
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-21 10:23:00 +0530

    Validate Binary Search Tree

--- 27_BinarySearchTree/01_leeetcode.cpp
+++ 27_BinarySearchTree/01_leeetcode.cpp
@@ -101,3 +101,82 @@
 //     }
 // };
 
+
+//or
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if(root->val<p->val && root->val<q->val) return lowestCommonAncestor(root->right,p,q);
+//         else if(root->val>p->val && root->val>q->val) return lowestCommonAncestor(root->left,p,q);
+//         else return root;
+//     }
+// };
+
+
+
+// 98. Validate Binary Search Tree
+
+// Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+// A valid BST is defined as follows:
+// The left subtree of a node contains only nodes with keys strictly less than the node's key.
+// The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
+ 
+// Example 1:
+// Input: root = [2,1,3]
+// Output: true
+
+//max method (Not that much good as it is not optimized and we are traversing the tree multiple times)
+
+// class Solution {
+// public:
+//     long long maxTree(TreeNode* root){
+//         if(root==NULL) return LLONG_MIN;
+//         else return max((long long)(root->val),max(maxTree(root->left),maxTree(root->right)));
+//     }
+//     long long minTree(TreeNode* root){
+//         if(root==NULL) return LLONG_MAX;
+//         else return min((long long)(root->val),min(minTree(root->left),minTree(root->right)));
+//     }
+//     bool isValidBST(TreeNode* root) {
+//         if(root==NULL) return true;
+//         if((long long)(root->val) <= maxTree(root->left)) return false;
+//         if((long long)(root->val) >= minTree(root->right)) return false;
+//         return isValidBST(root->left) && isValidBST(root->right);
+//     }
+// };
+
+//or
+
+// 98. Validate Binary Search Tree
+
+// Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+// A valid BST is defined as follows:
+// The left subtree of a node contains only nodes with keys strictly less than the node's key.
+// The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
+ 
+
+// Example 1:
+// Input: root = [2,1,3]
+// Output: true
+
+
+// class Solution {
+// public:
+//     void inorder(TreeNode* root, vector<int> &ans){
+//         if(root==NULL) return;
+//         inorder(root->left,ans);
+//         ans.push_back(root->val);
+//         inorder(root->right,ans);
+//     }   
+//     bool isValidBST(TreeNode* root) {
+//         vector<int> ans;
+//         inorder(root,ans);
+//         for(int i=1;i<ans.size();i++){
+//             if(ans[i]<=ans[i-1]) return false;
+//         }
+//         return true;
+        
+//     }
+// };
