7d61c53fa794d560ac7fa3e4dc178aa065b5fdd3
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-21 12:03:56 +0530

    Construct Binary Search Tree from Preorder Traversal

--- 27_BinarySearchTree/01_leeetcode.cpp
+++ 27_BinarySearchTree/01_leeetcode.cpp
@@ -264,3 +264,70 @@
 //         return helper(nums,0,n-1);
 //     }
 // };
+
+
+
+
+// 1008. Construct Binary Search Tree from Preorder Traversal
+
+// Given an array of integers preorder, which represents the preorder traversal of a BST (i.e., binary search tree), construct the tree and return its root.
+// It is guaranteed that there is always possible to find a binary search tree with the given requirements for the given test cases.
+// A binary search tree is a binary tree where for every node, any descendant of Node.left has a value strictly less than Node.val, and any descendant of Node.right has a value strictly greater than Node.val.
+// A preorder traversal of a binary tree displays the value of the node first, then traverses Node.left, then traverses Node.right.
+
+// Example 1:
+
+// Input: preorder = [8,5,1,7,10,12]
+// Output: [8,5,10,1,7,null,12]
+
+// class Solution {
+// public:
+//     TreeNode* build(vector<int>& pre,int prelo,int prehi, vector<int>& in,int inlo,int inhi){
+//         if(prelo>prehi) return NULL;
+//         TreeNode* root = new TreeNode(pre[prelo]);
+//         if(prelo==prehi) return root;
+//         int i=inlo;
+//         while(i<=inhi){
+//             if(in[i]==pre[prelo]) break;
+//             i++;
+//         }
+//         int leftCount=i-inlo;
+//         int rightCount=inhi-i;
+//         root->left=build(pre,prelo+1,prelo+leftCount,in,inlo,i-1);
+//         root->right=build(pre,prelo+leftCount+1,prehi,in,i+1,inhi);
+//         return root;
+//     }
+//     TreeNode* bstFromPreorder(vector<int>& pre) {
+//         vector<int> in =pre;//copy of array
+//         sort(in.begin(),in.end());//iise inorder ban jaega fir normally build kar do tree
+//          int n= pre.size();
+//         return build(pre,0,n-1,in,0,n-1);
+//     }
+// };
+
+
+// //or
+// class Solution {
+// public:
+//     void insert(TreeNode*& root, int val){
+//         if(root==NULL){
+//              root =new TreeNode(val);
+//              return;
+//         }
+//         else if(root->val>val){
+//             if(root->left==NULL) root->left =new TreeNode(val); 
+//             else  insert(root->left,val);
+//         }
+//         else if(root->val<val){
+//             if(root->right==NULL) root->right =new TreeNode(val); 
+//             else  insert(root->right,val);
+//         }
+//     }
+//     TreeNode* bstFromPreorder(vector<int>& pre) {
+//         TreeNode* root = new TreeNode(pre[0]);
+//         for(int i=1;i<pre.size();i++){
+//             insert(root,pre[i]);
+//         }       
+//         return root; 
+//     }
+// };
