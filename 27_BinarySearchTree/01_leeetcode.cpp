69bdb4a0fc7aca0764a8ba3ded9f5aaebb1c5f4c
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-21 08:09:06 +0530

    Insert into a Binary Search Tree

--- 27_BinarySearchTree/01_leeetcode.cpp
+++ 27_BinarySearchTree/01_leeetcode.cpp
@@ -25,3 +25,28 @@
 //         else  return searchBST(root->right,val);
 //     }
 // };
+
+
+
+// 701. Insert into a Binary Search Tree
+
+// You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+// Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+// Example 1:
+ 
+// class Solution {
+// public:
+//     TreeNode* insertIntoBST(TreeNode* root, int val) {
+//         if(root==NULL) return new TreeNode(val);
+//         else if(root->val>val){
+//             if(root->left==NULL) root->left =new TreeNode(val); //or TreeNode* temp = new TreeNode(val); root->left=temp; 
+//             else  insertIntoBST(root->left,val);
+//         }
+//         else if(root->val<val){
+//             if(root->right==NULL) root->right =new TreeNode(val); //or TreeNode* temp = new TreeNode(val); root->right=temp;
+//             else  insertIntoBST(root->right,val);
+//         }
+//         return root;
+//     }
+// };
