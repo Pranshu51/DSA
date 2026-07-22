4e07facb81fb8a339616679cdb8d7d6fb5ea63e2
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-22 16:50:01 +0530

    Trim a Binary Search Tree

--- 27_BinarySearchTree/02_DeletionOfNode.cpp
+++ 27_BinarySearchTree/02_DeletionOfNode.cpp
@@ -93,3 +93,46 @@ class Node{
 //         return root;
 //     }
 // };
+
+
+//L-3
+
+//669. Trim a Binary Search Tree
+
+// Given the root of a binary search tree and the lowest and highest boundaries as low and high, trim the tree so that all its elements lies in [low, high]. Trimming the tree should not change the relative structure of the elements that will remain in the tree (i.e., any node's descendant should remain a descendant). It can be proven that there is a unique answer.
+// Return the root of the trimmed binary search tree. Note that the root may change depending on the given bounds.
+// Example 1:
+// Input: root = [1,0,2], low = 1, high = 2
+// Output: [1,null,2]
+
+
+// class Solution {
+// public:
+//     void trim(TreeNode* root, int low, int high){
+//         if(root==NULL) return;
+//         while(root->left!=NULL){
+//             if(root->left->val<low){
+//                 root->left=root->left->right;
+//             }
+//             else if(root->left->val>high){
+//                 root->left=root->left->left;
+//             }else break;
+//         }
+//         while(root->right!=NULL){
+//             if(root->right->val>high){
+//                 root->right=root->right->left;
+//             }
+//             else if(root->right->val<low){
+//                 root->right=root->right->right;
+//             }else break;
+//         }
+//         trim(root->left,low,high);
+//         trim(root->right,low,high);
+//     }
+//     TreeNode* trimBST(TreeNode* root, int low, int high) {
+//         TreeNode* dummy = new TreeNode(INT_MAX);
+//         dummy->left = root;
+//         trim(dummy,low,high);
+//         return dummy->left;
+//     }
+// };
