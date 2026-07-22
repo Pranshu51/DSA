3b88396b22bcd365870d30de60ae55801eaf8a38
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-22 15:40:52 +0530

    Delete Node in a BST

--- 27_BinarySearchTree/02_DeletionOfNode.cpp
+++ 27_BinarySearchTree/02_DeletionOfNode.cpp
@@ -28,3 +28,68 @@ class Node{
 //1. If the node is a leaf node, simply remove it from the tree.
 //2.It has only one child(either the right or left)->easy.
 //3.If it has 2 child nodes ->hard
+
+
+
+// 450. Delete Node in a BST
+
+// Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+// Basically, the deletion can be divided into two stages:
+// Search for a node to remove.
+// If the node is found, delete the node.
+
+// Example 1:
+// Input: root = [5,3,6,2,4,null,7], key = 3
+// Output: [5,4,6,2,null,null,7]
+// Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+// One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+// Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+// class Solution {
+// public:
+// //pred
+//     TreeNode * iop(TreeNode * root){
+//         TreeNode* pred =root->left;
+//         while(pred->right!=NULL){
+//             pred = pred->right;
+//         }
+//         return pred;
+//     }
+//     //succ
+//     TreeNode * ios(TreeNode * root){
+//         TreeNode* succ =root->right;
+//         while(succ->left!=NULL){
+//             succ = succ->left;
+//         }
+//         return succ;
+//     }
+//     TreeNode* deleteNode(TreeNode* root, int key) {
+//         if(root==NULL) return NULL;
+//         if(root->val==key){
+//             //Case 1: NO Child
+//             if(root->left==NULL && root->right==NULL) return NULL;
+
+//             //case 2: 1 Child:
+//             if(root->left==NULL || root->right==NULL){
+//                 if(root->left!=NULL) return root->left;
+//                 else return root->right;
+//             } 
+
+//             //Case 3: 2 Child:
+//             if(root->left!=NULL && root->right!=NULL){
+//                 //replcae it with root predecessor/successor
+//                 //delete the pred/succ
+//                 TreeNode * pred = iop(root);
+//                 root->val = pred->val;
+//                 root->left = deleteNode(root->left,pred->val);
+//             }
+//         }
+//         else if(root->val>key){//go left
+//             root->left = deleteNode(root->left,key);
+//         }
+//         else {//go right
+//             root->right = deleteNode(root->right,key);
+//         }
+//         return root;
+//     }
+// };
