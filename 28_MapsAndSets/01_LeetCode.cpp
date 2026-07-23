cfc1f2c52f9023f5ccf3b11464581e3d2697410a
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-23 19:03:53 +0530

    Unique Number of Occurrences

--- 28_MapsAndSets/01_LeetCode.cpp
+++ 28_MapsAndSets/01_LeetCode.cpp
@@ -277,4 +277,35 @@
 //        }
 //        return ans;
 //     }
+// }; 
+
+
+// 1207. Unique Number of Occurrences
+
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+// Example 1:
+
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+
+
+// class Solution {
+// public:
+//     bool uniqueOccurrences(vector<int>& arr) {
+//         int n = arr.size();
+//         unordered_map<int,int> m;
+//         for(int i=0;i<n;i++){
+//             m[arr[i]]++;
+//         }
+//         unordered_set<int> s;
+//         for(auto x: m){
+//             int freq = x.second;
+//             if(s.find(freq) != s.end()) return false;
+//             else s.insert(freq);
+//         }
+//         return true;
+//     }
 // };
