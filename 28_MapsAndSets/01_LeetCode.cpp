f4ee54375a4830db2394a643d528c35d44a35f9b
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-23 18:52:23 +0530

    Two Sum with map

--- 28_MapsAndSets/01_LeetCode.cpp
+++ 28_MapsAndSets/01_LeetCode.cpp
@@ -207,3 +207,74 @@
 //         return true;
 //     }
 // };
+
+
+
+// 1. Two Sum
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int> ans;
+//         int n= nums.size();
+//         map<int,int>mp;
+//         for(int i=0;i<n;i++){
+//             int rem =target-nums[i];
+//             if(mp.find(rem)!=mp.end()){
+//                 ans.push_back(mp[rem]);
+//                 ans.push_back(i);
+//             }
+//             else mp[nums[i]] =i;
+//         }
+//         return ans;
+//     }
+// };
+
+// //or
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n= nums.size();
+//         map<int,int>mp;
+//         for(int i=0;i<n;i++){
+//             int rem =target-nums[i];
+//             if(mp.find(rem)!=mp.end()){
+//                 int j =mp[rem];
+//                 return {j,i};
+//             }
+//             mp[nums[i]] =i;
+//         }
+//         return {};
+//     }
+// };
+
+
+
+// //brute force 
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//        vector<int> ans;
+//        int n =nums.size();
+//        for(int i=0;i<n-1;i++){
+//         for(int j=i+1;j<n;j++){
+//             if(nums[i]+nums[j]==target){
+//                 ans.push_back(i);
+//                 ans.push_back(j);
+//                 return ans;
+//             }
+//         }
+//        }
+//        return ans;
+//     }
+// };
