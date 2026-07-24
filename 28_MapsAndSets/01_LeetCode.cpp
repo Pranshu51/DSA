5bda61764f0f58d13ee8c463c31e55cd88770faf
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-24 05:57:37 +0530

    valid Anagram

--- 28_MapsAndSets/01_LeetCode.cpp
+++ 28_MapsAndSets/01_LeetCode.cpp
@@ -184,30 +184,36 @@
 // };
 
 //or
-// class Solution {
+// /class Solution {
 // public:
 //     bool isAnagram(string s, string t) {
 //         if(s.length()!=t.length()) return false;
 //         unordered_map<char,int> map1;
-//         unordered_map<char,int> map2;
 //         for(int i=0;i<s.size();i++){
 //             map1[s[i]]++;
 //         }
-//         for(int i=0;i<t.size();i++){
-//             map2[t[i]]++;
-//         }
-//         for(auto x:map1){
-//             char ch1=x.first;
-//             int freq1=x.second;
-//             if(map2.find(ch1)!=map2.end()){
-//                 int freq2 = map2[ch1];
-//                 if(freq1!=freq2) return false;
-//             }else return false;
+//         for(int i =0;i<t.size();i++){
+//             char ch= t[i];
+//             if(map1.find(ch)!=map1.end()){
+//                 map1[ch]--;
+//                 if(map1[ch]==0) map1.erase(ch);
+//             }
+//             else return false;
 //         }
 //         return true;
 //     }
 // };
 
+//or older way
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//          sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         if(s==t) return true;
+//         else return false;
+//     }
+// };
 
 
 // 1. Two Sum
