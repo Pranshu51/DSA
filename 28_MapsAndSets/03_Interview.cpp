0ff6d75373f5768f9f85b3bb49b06f224e4a8cc4
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-28 15:19:18 +0530

    Determine if Two Strings Are Close

--- 28_MapsAndSets/03_Interview.cpp
+++ 28_MapsAndSets/03_Interview.cpp
@@ -178,3 +178,100 @@
 //          return ans;
 //     }
 // };
+
+
+
+
+
+// 1657. Determine if Two Strings Are Close
+
+// Two strings are considered close if you can attain one from the other using the following operations:
+
+// Operation 1: Swap any two existing characters.
+// For example, abcde -> aecdb
+// Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+// For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+// You can use the operations on either string as many times as necessary.
+
+// Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise. 
+
+// Example 1:
+
+// Input: word1 = "abc", word2 = "bca"
+// Output: true
+// Explanation: You can attain word2 from word1 in 2 operations.
+// Apply Operation 1: "abc" -> "acb"
+// Apply Operation 1: "acb" -> "bca"
+
+// class Solution {
+// public:
+//     bool closeStrings(string str1, string str2) {
+//         if(str1.length()!=str2.length()) return false;
+//         unordered_map<char,int> m1;
+//         unordered_map<char,int> m2;
+//         for( auto x : str1){
+//             m1[x]++;
+//         }
+//         for( auto a : str2){
+//             m2[a]++;
+//         }
+//         unordered_map<int,int> s1;
+//         unordered_map<int,int> s2;
+//         for(auto x : m1){
+//             char ch = x.first;
+//             if(m2.find(ch)==m2.end()) return false;
+//         }
+//         for(auto b : m1){
+//             int s = b.second;
+//             s1[s]++;
+//         }
+//         for(auto b : m2){
+//             int s = b.second;
+//             s2[s]++;
+//         }
+//         for(auto x: s1){
+//             int key = x.first;
+//             if(s2.find(key)==s2.end()) return false;
+//             if(s1[key]!=s2[key]) return false;
+//         }
+//         return true;
+//     }
+// };
+
+
+
+
+
+// //or
+
+
+// class Solution {
+// public:
+//     bool closeStrings(string str1, string str2) {
+//         if(str1.length()!=str2.length()) return false;
+//         unordered_map<char,int> m1,m2;
+//         for(int i =0;i<str1.length();i++){
+//             m1[str1[i]]++;
+//             m2[str2[i]]++;
+//         }
+//         unordered_map<int,int> s1,s2;
+//         for(auto x : m1){
+//             char ch = x.first;
+//             if(m2.find(ch)==m2.end()) return false;
+//         }
+//         for(auto b : m1){
+//             int s = b.second;
+//             s1[s]++;
+//         }
+//         for(auto b : m2){
+//             int s = b.second;
+//             s2[s]++;
+//         }
+//         for(auto x: s1){
+//             int key = x.first;
+//             if(s2.find(key)==s2.end()) return false;
+//             if(s1[key]!=s2[key]) return false;
+//         }
+//         return true;
+//     }
+// };
