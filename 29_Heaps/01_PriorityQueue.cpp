04d0d6b821a444e41d5efaf7c245dd89828f43e2
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-29 16:02:21 +0530

    Kth Largest Element in an Array

--- 29_Heaps/01_PriorityQueue.cpp
+++ 29_Heaps/01_PriorityQueue.cpp
@@ -41,17 +41,17 @@ using namespace std;
 // } 
 
 // Min heap implementation
-int main(){
-    //create a  min heap
-    priority_queue<int, vector<int>, greater<int>> pq;
-    pq.push(10);
-    pq.push(-6);
-    pq.push(-10);
-    cout<<pq.top()<<endl;//-10
-    pq.pop();//-10 pop
-    cout<<pq.top();//-6
+// int main(){
+//     //create a  min heap
+//     priority_queue<int, vector<int>, greater<int>> pq;
+//     pq.push(10);
+//     pq.push(-6);
+//     pq.push(-10);
+//     cout<<pq.top()<<endl;//-10
+//     pq.pop();//-10 pop
+//     cout<<pq.top();//-6
 
-}
+// }
 
 
 //Two types of HEAPS:
@@ -74,4 +74,78 @@ int main(){
 //kth smallest,largest , top k frequent elements,closest k
 // 2. at any point min, and max elements are required
 //3.sorting(heap)
+
+//QUE=> FIND THE KTH SMALLEST ELEMENT IN THE ARRAY
+//Method 1: Builtin Sort the array and return the kth (arr[k-1])element. T.C => O(nlogn) S.C => O(logn) for sorting
+//Method 2: selection sort T.C=> O(k*n) S.C=O(1)
+//Method 3:Quick Select T.c=>O(n)
+//Method 4:Using head T.c=>O(nlogk) S.C=>O(k)
+
+//NOTE: agr kth largest dia h to maxheap , aur kth smallest dia h to min heap use karenge
+
+
+
+// int main(){
+//     int arr[] = {10,20,-4,5,18,24,1,-7,56};
+//     int k=4;
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     priority_queue<int> pq;
+//     for(int i=0;i<n;i++){
+//         pq.push(arr[i]);//t.c=>O(logk)
+//         if(pq.size() > k){
+//             pq.pop();//t.c=>O(logk)
+//         }
+//     }
+//     cout << "The " << k << "th smallest element is: " << pq.top() << endl;
+
+// }//T.C => O(nlogk) S.C(Auxillary space) => O(k)else S.C= O(n)
+
+// Que=> Find the Kth largest element in the array
+
+int main(){
+    int arr[] = {10,20,-4,5,18,24,1,-7,56};
+    int k=2;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(int i=0;i<n;i++){
+        pq.push(arr[i]);//t.c=>O(logk)
+        if(pq.size() > k){
+            pq.pop();//t.c=>O(logk)
+        }
+    }
+    cout << "The " << k << "th largest element is: " << pq.top() << endl;
+
+}//T.C => O(nlogk) S.C(Auxillary space) => O(k)else S.C= O(n)
+
+
+
+// 215. Kth Largest Element in an Array
+
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// Can you solve it without sorting?
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,1,5,6,4], k = 2
+// Output: 5
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         priority_queue<int, vector<int>, greater<int>> pq;
+//         for(int i=0;i<n;i++){
+//             pq.push(nums[i]);//t.c=>O(logk)
+//             if(pq.size() > k){
+//                 pq.pop();//t.c=>O(logk)
+//             }
+//         }
+//         return pq.top();//t.c=>O(n*logk) s.c =O(k)
+//     }
+// };
+
 
