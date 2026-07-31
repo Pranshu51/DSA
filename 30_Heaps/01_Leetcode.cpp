0667f06ff5e5c62f77882fae19b7a20721e48a14
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-31 12:47:51 +0530

    Find K Closest Elements

--- 30_Heaps/01_Leetcode.cpp
+++ 30_Heaps/01_Leetcode.cpp
@@ -35,3 +35,80 @@
 // pending
 
 
+
+
+
+
+// 1636. Sort Array by Increasing Frequency
+
+// Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+// Return the sorted array.
+
+// Example 1:
+
+// Input: nums = [1,1,2,2,2,3]
+// Output: [3,1,1,2,2,2]
+// Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
+
+// class Solution {
+// public:
+//     typedef pair<int, int> pi;
+
+//     vector<int> frequencySort(vector<int>& nums) {
+//         unordered_map<int, int> mp;
+
+//         // Count frequency
+//         for (auto x : nums) {
+//             mp[x]++;
+//         }
+//         //using comparator
+//         auto cmp = [&mp](int a ,int b){
+//             if(mp[a]!=mp[b]){
+//                 return mp[a]<mp[b];
+//             }else return a>b;
+//         };
+
+//         sort(nums.begin(),nums.end(),cmp);
+//         return nums;
+//     }
+// };
+
+
+
+
+// 658. Find K Closest Elements
+
+// Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+// An integer a is closer to x than an integer b if:
+
+// |a - x| < |b - x|, or
+// |a - x| == |b - x| and a < b
+ 
+
+// Example 1:
+
+// Input: arr = [1,2,3,4,5], k = 4, x = 3
+
+// Output: [1,2,3,4]
+
+
+// class Solution {
+// public:
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         vector<int> ans;
+//         int n =arr.size();
+//         priority_queue<pair<int,int>> pq;
+//         for(int i=0;i<n;i++){
+//             int diff = abs(arr[i]-x);
+//             pq.push({diff,arr[i]});
+//             if(pq.size()>k) pq.pop();
+//         }
+//         while(pq.size()>0){
+//             ans.push_back(pq.top().second);
+//             pq.pop();
+//         }
+//         sort(ans.begin(),ans.end());
+//         return ans;
+//     }
+// };
