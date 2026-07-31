0f1a57eb81dc8dd3c4f129b409f5182d33ee3554
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-07-31 13:44:56 +0530

    K Closest Points to Origin

--- 30_Heaps/01_Leetcode.cpp
+++ 30_Heaps/01_Leetcode.cpp
@@ -112,3 +112,65 @@
 //         return ans;
 //     }
 // };
+
+
+
+
+
+
+// 973. K Closest Points to Origin
+
+// Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+// The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+// You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+
+// Example 1:
+
+// Input: points = [[1,3],[-2,2]], k = 1
+// Output: [[-2,2]]
+// Explanation:
+// The distance between (1, 3) and the origin is sqrt(10).
+// The distance between (-2, 2) and the origin is sqrt(8).
+// Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+// We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
+
+// class Solution {
+// public:
+//     vector<vector<int>> kClosest(vector<vector<int>>& arr, int k) {
+//         vector<vector<int>> ans;
+//         priority_queue<pair<int,vector<int>>> pq;
+//         for(auto v: arr){
+//             int x= v[0];
+//             int y= v[1];
+//             int dist = x*x+y*y;
+//             pq.push({dist,v});
+//             if(pq.size()>k) pq.pop();
+//         }
+//         while(pq.size()>0){
+//             ans.push_back(pq.top().second);
+//             pq.pop();
+//         }
+//         return ans;
+//     }
+// };
+
+// //or
+// class Solution {
+// public:
+//     vector<vector<int>> kClosest(vector<vector<int>>& arr, int k) {
+//         vector<vector<int>> ans;
+//         priority_queue<pair<int,vector<int>>> pq;
+//         for(int i=0;i<arr.size();i++){
+//             int x= arr[i][0];
+//             int y= arr[i][1];
+//             int dist = x*x+y*y;
+//             pq.push({dist,arr[i]});
+//             if(pq.size()>k) pq.pop();
+//         }
+//         while(pq.size()>0){
+//             ans.push_back(pq.top().second);
+//             pq.pop();
+//         }
+//         return ans;
+//     }
+// };
