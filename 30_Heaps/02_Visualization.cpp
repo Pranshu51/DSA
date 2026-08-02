5e5ae39e0f3ac8556d57480bbb72e36408a9cd14
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-02 11:00:59 +0530

    HEAPIFY ALGORITHM

--- 30_Heaps/02_Visualization.cpp
+++ 30_Heaps/02_Visualization.cpp
@@ -414,3 +414,55 @@ using namespace std;
 
 //     return 0;
 // }
+
+
+
+// HEAPIFY ALGORITHM
+//For even : leaf nodes are n/2
+//For odd : leaf nodes are n/2+1
+
+
+// void print(int arr[],int n){
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+
+
+// void heapify(int arr[],int n,int i){
+//    //same pop rearrangement
+//         while(true){
+//             int left = 2*i;
+//             int right = 2*i + 1;
+//             if(left>=n) break; // if left child index is out of bounds, break
+//             if(right>=n){
+//                 if(arr[left]<arr[i]){
+//                     swap(arr[left],arr[i]);
+//                     i = left;
+//                 }
+//                 break;
+//             }
+//             if(arr[left]<arr[right] ){
+//                 if(arr[left]<arr[i]){
+//                 swap(arr[left],arr[i]);
+//                 i = left;
+//                 }else break;
+//             }else{
+//                 if(arr[right]<arr[i]){
+//                     swap(arr[right],arr[i]);
+//                     i = right;
+//                 }else break;
+//             }
+//         }
+// }
+
+// int main(){
+//     int arr[] = {-1,10,2,14,11,1,4};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     print(arr,n);
+//     for(int i=n/2;i>0;i--){
+//         heapify(arr,n,i);
+//     }
+//     print(arr,n);
+// }
