459c4068311aa4e476df09533fb085dc7353c9d9
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-06 14:39:31 +0530

    Construct String With Repeat Limit

--- 31_Greedy/02_Leetcode.cpp
+++ 31_Greedy/02_Leetcode.cpp
@@ -128,3 +128,61 @@
 // };
 
 
+
+
+
+
+
+// 2182. Construct String With Repeat Limit
+
+// You are given a string s and an integer repeatLimit. Construct a new string repeatLimitedString using the characters of s such that no letter appears more than repeatLimit times in a row. You do not have to use all characters from s.
+// Return the lexicographically largest repeatLimitedString possible.
+// A string a is lexicographically larger than a string b if in the first position where a and b differ, string a has a letter that appears later in the alphabet than the corresponding letter in b. If the first min(a.length, b.length) characters do not differ, then the longer string is the lexicographically larger one.
+
+ 
+
+// Example 1:
+
+// Input: s = "cczazcc", repeatLimit = 3
+// Output: "zzcccac"
+// Explanation: We use all of the characters from s to construct the repeatLimitedString "zzcccac".
+// The letter 'a' appears at most 1 time in a row.
+// The letter 'c' appears at most 3 times in a row.
+// The letter 'z' appears at most 2 times in a row.
+// Hence, no letter appears more than repeatLimit times in a row and the string is a valid repeatLimitedString.
+// The string is the lexicographically largest repeatLimitedString possible so we return "zzcccac".
+// Note that the string "zzcccca" is lexicographically larger but the letter 'c' appears more than 3 times in a row, so it is not a valid repeatLimitedString.
+
+// class Solution {
+// public:
+//     string repeatLimitedString(string s, int repeatLimit) {
+//         unordered_map<char,int> mp;
+//         for(auto x: s){
+//             mp[x]++;
+//         }
+//         priority_queue<pair<char,int>> pq;
+//         for(auto x: mp){
+//             pq.push(x);
+//         }
+//         string result="";
+//         while(!pq.empty()){
+//             auto largest = pq.top();
+//             pq.pop();
+//             int len = min(largest.second,repeatLimit);
+//             for(int i=0;i<len;i++){
+//                 result+=largest.first;
+//             }
+//             pair<char,int> secondLargest;
+//             if(largest.second-len>0){
+//                 if(!pq.empty()){
+//                 secondLargest = pq.top();
+//                 pq.pop();
+//                 result+=secondLargest.first;
+//                 }else return result;
+//                 if(secondLargest.second-1>0) pq.push({secondLargest.first,secondLargest.second-1});
+//                 pq.push({largest.first,largest.second-len});
+//             }
+//         }
+//         return result;
+//     }
+// };
