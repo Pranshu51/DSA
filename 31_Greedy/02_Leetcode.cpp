939ac9f795609a46c91e9e535be60f258cca74ab
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-06 19:45:06 +0530

    Meeting Rooms II

--- 31_Greedy/02_Leetcode.cpp
+++ 31_Greedy/02_Leetcode.cpp
@@ -266,3 +266,35 @@
 //     }         
 // };
 
+
+
+//or
+
+// class Solution {
+//   public:
+//     int minMeetingRooms(vector<int> &start, vector<int> &end) {
+//         // code here
+//         sort(start.begin(),start.end());
+//         sort(end.begin(),end.end());
+        
+//         int ans=0;
+//         int room=0;
+//         int i=0;int j=0;
+        
+//         while(i<start.size() && j<end.size()){
+//             if(start[i]<end[j]){
+//                 room++;
+//                 ans=max(room,ans);
+//                 i++;
+//             }else if(start[i]>end[j]){
+//                 room--;
+//                 j++;
+//             }else{
+//                 i++;
+//                 j++;
+//             }
+//         }
+//         return ans;
+//     }         
+// };
+
