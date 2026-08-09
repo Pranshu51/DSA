08d7d2f872ed6c167e002f8e45a96ee3adde9ae3
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-10 03:18:38 +0530

    Longest Substring Without Repeating Characters

--- 19_SlidingWindow/02_leetcode.cpp
+++ 19_SlidingWindow/02_leetcode.cpp
@@ -329,3 +329,43 @@ using namespace std;
 //         return result;
 //     }
 // };
+
+
+
+
+// 3. Longest Substring Without Repeating Characters
+
+// Given a string s, find the length of the longest substring without duplicate characters.
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n = s.length();
+//         int i = 0, j = 0;
+//         int maxLen = 0;
+//         unordered_set<char> st;
+
+//         while (j < n) {
+//             // Shrink window from the left until incoming character s[j] is unique
+//             while (st.count(s[j])) {
+//                 st.erase(s[i]);
+//                 i++;
+//             }
+
+//             // Add s[j] to set and update maximum length
+//             st.insert(s[j]);
+//             maxLen = max(maxLen, j - i + 1);
+
+//             j++; // Expand window to the right
+//         }
+
+//         return maxLen;
+//     }
+// };
