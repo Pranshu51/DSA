a46dd705d6d980d41e2fff93ad08353a9f96dee4
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-10 04:01:56 +0530

    Continuous Subarray Sum

--- 18_PrefixSum/02_leetcode.cpp
+++ 18_PrefixSum/02_leetcode.cpp
@@ -31,6 +31,41 @@
 //     }
 // };
 
+// 523. Continuous Subarray Sum
+
+// Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+// A good subarray is a subarray where:
+// its length is at least two, and
+// the sum of the elements of the subarray is a multiple of k.
+// Note that:
+
+// A subarray is a contiguous part of the array.
+// An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+
+// Example 1:
+
+// Input: nums = [23,2,4,6,7], k = 6
+// Output: true
+// Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
+// class Solution {
+// public:
+//     bool checkSubarraySum(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         unordered_map<int,int> mp;
+//         int sum =0;
+//         mp[0] =-1;
+
+//         for(int i=0;i<n;i++){
+//             sum+=nums[i];
+//             int rem = sum%k;
+//             if(mp.find(rem) !=mp.end()){
+//                 if(i-mp[rem]>=2) return true;
+//             }else mp[rem] =i;
+//         }
+//         return false;
+//     }
+// };
+
 // 1480. Running Sum of 1d Array
 
 // Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
