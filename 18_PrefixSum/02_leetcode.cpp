4b7860219deea9e0d6e294106077af1b586acaea
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-10 03:33:15 +0530

    Find Pivot Index

--- 18_PrefixSum/02_leetcode.cpp
+++ 18_PrefixSum/02_leetcode.cpp
@@ -1,3 +1,36 @@
+// 724. Find Pivot Index
+
+// Given an array of integers nums, calculate the pivot index of this array.
+// The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+// If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+// Return the leftmost pivot index. If no such index exists, return -1.
+
+// Example 1:
+
+// Input: nums = [1,7,3,6,5,6]
+// Output: 3
+// Explanation:
+// The pivot index is 3.
+// Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+// Right sum = nums[4] + nums[5] = 5 + 6 = 11
+// class Solution {
+// public:
+//     int pivotIndex(vector<int>& nums) {
+//         int n=nums.size();
+//         int leftSum=0;
+//         int totalSum=0;
+//         for(int i=0;i<n;i++){
+//              totalSum+=nums[i];
+//         }
+//         for(int i=0;i<n;i++){
+//             int rightSum=totalSum-nums[i]-leftSum;
+//             if(leftSum==rightSum) return i;
+//             leftSum+=nums[i];
+//         }
+//         return -1;  
+//     }
+// };
+
 // 1480. Running Sum of 1d Array
 
 // Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
