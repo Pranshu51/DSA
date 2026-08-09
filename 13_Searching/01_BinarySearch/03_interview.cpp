20a6e4967c54cfe53091cd0014b34bddc5bd440d
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-09 11:23:28 +0530

    Search a 2D Matrix

--- 13_Searching/01_BinarySearch/03_interview.cpp
+++ 13_Searching/01_BinarySearch/03_interview.cpp
@@ -24,6 +24,38 @@
 // };
 
 
+// 74. Search a 2D Matrix
+
+// You are given an m x n integer matrix matrix with the following two properties:
+
+// Each row is sorted in non-decreasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+
+// You must write a solution in O(log(m * n)) time complexity.
+
+// Example 1:Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int m = matrix.size();//row
+//         int n = matrix[0].size();//col
+//         if(m==0)  return false;
+//         int start =0; int end = m*n-1;
+//         while(start<=end){
+//             int mid = start + (end - start) / 2;;
+//             int element = matrix[mid/n][mid%n];//mid/n=>row,mid%n=>col
+//             if(element==target) return true;
+//             else if(target>element){
+//                 start = mid+1;
+//             }else end = mid-1;
+//         }
+//         return false;
+//     }
+// };
 
 
 
