f131b98ce51cc71f968dad4d84db6eae33be6260
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-12 13:39:16 +0530

    Remove Zero Sum Consecutive Nodes from Linked List

--- 23_LinkedList/05_leetcode.cpp
+++ 23_LinkedList/05_leetcode.cpp
@@ -573,3 +573,47 @@
 //        return root;
 //     }
 // };
+
+
+
+// 1171. Remove Zero Sum Consecutive Nodes from Linked List
+
+// Given the head of a linked list, we repeatedly delete consecutive sequences of nodes that sum to 0 until there are no such sequences.
+// After doing so, return the head of the final linked list.  You may return any such answer.
+
+// (Note that in the examples below, all sequences are serializations of ListNode objects.)
+
+// Example 1:
+
+// Input: head = [1,2,-3,3,1]
+// Output: [3,1]
+// Note: The answer [1,2,1] would also be accepted.
+
+// class Solution {
+// public:
+//     ListNode* removeZeroSumSublists(ListNode* head) {
+//         unordered_map<int,ListNode*> mp;
+//         ListNode* dummy = new ListNode(0);
+//         dummy->next=head;
+//         mp[0] = dummy;
+//         int prefixSum =0;
+//         while(head!=NULL){
+//             prefixSum+=head->val;
+//             if(mp.find(prefixSum)!=mp.end()){
+//                 ListNode* start = mp[prefixSum];
+//                 ListNode* temp = start;
+//                 int pSum = prefixSum;
+//                 while(temp!=head){
+//                     temp = temp->next;
+//                     pSum+=temp->val;
+//                     if (temp!=head) mp.erase(pSum);
+//                 }
+//                 start->next = head->next;
+//             }else{
+//                 mp[prefixSum] = head;
+//             }
+//             head = head->next;
+//         }
+//         return dummy->next;
+//     }
+// };
