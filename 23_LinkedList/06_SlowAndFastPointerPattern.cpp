3fa6a8090a4fdbfb98a89a0f21f4ef66b18b8374
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-12 11:02:54 +0530

    Linked List Cycle II

--- 23_LinkedList/06_SlowAndFastPointerPattern.cpp
+++ 23_LinkedList/06_SlowAndFastPointerPattern.cpp
@@ -45,3 +45,34 @@
 //         return slow==1;
 //     }
 // };
+
+
+
+
+// 142. Linked List Cycle II
+
+// Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+// Do not modify the linked list.
+
+// Example 1
+// Input: head = [3,2,0,-4], pos = 1
+// Output: tail connects to node index 1
+// Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         if(head==NULL || head->next==NULL) return NULL;
+//         unordered_set<ListNode*> st;
+//         ListNode* fast = head;
+//         while(fast!=NULL){
+//             if(st.find(fast)!= st.end()) return fast;
+//             else {
+//                 st.insert(fast);
+//                 fast=fast->next;
+//             }
+//         }
+//         return NULL;
+//     }
+// };
