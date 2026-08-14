8544b41d72e240c4a4f7554ba4b43d8a4f8c3588
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-14 11:43:49 +0530

    Maximum Twin Sum of a Linked List

--- 23_LinkedList/05_leetcode.cpp
+++ 23_LinkedList/05_leetcode.cpp
@@ -729,3 +729,102 @@
 //         return temp;     
 //     }
 // };
+
+
+// 2130. Maximum Twin Sum of a Linked List
+
+// In a linked list of size n, where n is even, the ith node (0-indexed) of the linked list is known as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
+// For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2. These are the only nodes with twins for n = 4.
+// The twin sum is defined as the sum of a node and its twin.
+// Given the head of a linked list with even length, return the maximum twin sum of the linked list.
+
+// Example 1:
+
+// Input: head = [5,4,2,1]
+// Output: 6
+// Explanation:
+// Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 6.
+// There are no other nodes with twins in the linked list.
+// Thus, the maximum twin sum of the linked list is 6.
+
+
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         vector<int> v;
+
+//         while(head!=NULL){v
+//             v.push_back(head->val);
+//             head=head->next;
+//         }
+//         int i =0; int j =v.size()-1;
+//          int maxSum =0;
+//         while(i<j){
+//             maxSum = max(maxSum,v[i]+v[j]);
+//             i++;
+//             j--;
+//         }
+//         return maxSum;
+//     }
+// };
+
+
+// //or with stack
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         stack<int> st;
+//         ListNode* temp =head;
+//         while(temp!=NULL){
+//             st.push(temp->val);
+//             temp=temp->next;
+//         }
+//         int maxSum=0;
+//         int count =1;
+//         temp =head;
+//         int n = st.size();
+//         while(count <=n/2){
+//             maxSum = max(maxSum,temp->val+st.top());
+//             st.pop();
+//             temp =temp->next;
+//             count++;
+//         }
+//         return maxSum;
+//     }
+// };
+
+
+//or
+
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         ListNode* slow =head;
+//         ListNode* fast =head;
+//         while(fast!=NULL  && fast->next!=NULL){
+//             slow=slow->next;
+//             fast=fast->next->next;
+//         }
+//        ListNode* mid = slow;
+//        ListNode* prev = NULL;
+//        ListNode* nextNode = NULL;
+
+//        //2.Reverse from mid to end
+//        while(mid!=NULL){
+//         nextNode = mid->next;
+//         mid->next =prev;
+//         prev = mid;
+//         mid = nextNode;
+//        }
+
+//        //findMaxSum
+//         int maxSum =0;
+//         ListNode* temp = head;
+//        while(prev!=NULL){
+//             maxSum =max(maxSum,temp->val+prev->val);
+//             temp= temp->next;
+//             prev= prev->next;
+//        }
+//         return maxSum;
+//     }
+// };
