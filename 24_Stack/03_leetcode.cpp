e9fd35d2f9a1c8f5283fa36428cbda1d12b402aa
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-16 10:28:28 +0530

    Evaluate Reverse Polish Notation

--- 24_Stack/03_leetcode.cpp
+++ 24_Stack/03_leetcode.cpp
@@ -628,3 +628,57 @@
 //         return sum;
 //     }
 // };
+
+
+
+// 150. Evaluate Reverse Polish Notation
+
+// You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+// Evaluate the expression. Return an integer that represents the value of the expression.
+
+// Note that:
+
+// The valid operators are '+', '-', '*', and '/'.
+// Each operand may be an integer or another expression.
+// The division between two integers always truncates toward zero.
+// There will not be any division by zero.
+// The input represents a valid arithmetic expression in a reverse polish notation.
+// The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+
+// Example 1:
+
+// Input: tokens = ["2","1","+","3","*"]
+// Output: 9
+// Explanation: ((2 + 1) * 3) = 9
+
+// class Solution {
+// public:
+//     int getRes(int a , int b , string x){
+//         if(x=="+") return a+b;
+//         if(x=="-") return a-b;
+//         if(x=="*") return (long long)(a)*(long long)(b);
+//         if(x=="/") return a/b;
+//         return -1;
+//     }
+//     int evalRPN(vector<string>& tokens) {
+//         stack<int> st;
+//         for(string &x: tokens){
+//             if(x=="+" || x=="-"  || x=="*"  || x=="/"){
+//                 int b = st.top();
+//                 st.pop();
+
+//                 int a = st.top();
+//                 st.pop();
+
+//                 int result = getRes(a,b,x);
+
+//                 st.push(result);
+//             }else{
+//                 st.push(stoi(x));
+//             }
+//         }
+//         return st.top();
+//     }
+// };
