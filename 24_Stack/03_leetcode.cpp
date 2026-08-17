f903d5a262a82a3f190dfa800252dd7531d6c457
Pranshu Tiwari <pranshutiwari560@gmail.com>
2026-08-17 22:29:47 +0530

    Asteroid Collision

--- 24_Stack/03_leetcode.cpp
+++ 24_Stack/03_leetcode.cpp
@@ -959,3 +959,51 @@
 //         return result;
 //     }
 // };
+
+
+// 735. Asteroid Collision
+
+// We are given an array asteroids of integers representing asteroids in a row. The indices of the asteroid in the array represent their relative position in space.
+// For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+// Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+// Example 1:
+
+// Input: asteroids = [5,10,-5]
+// Output: [5,10]
+// Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+// Example 2:
+
+// Input: asteroids = [8,-8]
+// Output: []
+// Explanation: The 8 and -8 collide exploding each other.
+
+// class Solution {
+// public:
+//     vector<int> asteroidCollision(vector<int>& asteroids) {
+//         vector<int> result;
+
+//         for (int ast : asteroids) {
+//             bool destroyed = false;
+
+//             while (!result.empty() && result.back() > 0 && ast < 0) {
+//                 if (result.back() < -ast) {
+//                     result.pop_back();
+//                 } else if (result.back() == -ast) {
+//                     result.pop_back();
+//                     destroyed = true;
+//                     break;
+//                 } else {
+//                     destroyed = true;
+//                     break;
+//                 }
+//             }
+
+//             if (!destroyed) {
+//                 result.push_back(ast);
+//             }
+//         }
+
+//         return result;
+//     }
+// };
